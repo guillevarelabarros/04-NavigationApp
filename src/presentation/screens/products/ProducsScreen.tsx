860fb1da3f06a21e29fad59c4b41a3d44939671b
Guillermo I. Varela Barros <guillevarelabarros@gmail.com>
2026-02-20 17:01:15 -0300

    refactor: restructure app components and add initial screens

---
+++ src/presentation/screens/products/ProducsScreen.tsx
@@ -0,0 +1,11 @@
+
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export const ProductsScreen = () => {
+  return (
+    <View>
+      <Text>ProductsScreen</Text>
+    </View>
+  );
+};
---
+++ src/presentation/screens/tabs/Tab1Screen.tsx
@@ -0,0 +1,11 @@
+
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export const Tab1Screen = () => {
+  return (
+    <View>
+      <Text>Tab1Screen</Text>
+    </View>
+  );
+};
