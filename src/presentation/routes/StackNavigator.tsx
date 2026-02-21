e89b911b4fe66b273e7572a651cda66b7b8722b8
Guillermo I. Varela Barros <guillevarelabarros@gmail.com>
2026-02-21 14:54:03 -0300

    feat: add ProductScreen and integrate navigation in ProductsScreen

--- src/presentation/routes/StackNavigator.tsx
+++ src/presentation/routes/StackNavigator.tsx
@@ -3,20 +3,29 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { HomeScreen } from '../screens/home/HomeScreen';
 import { ProductsScreen } from '../screens/products/ProducsScreen';
 import { SettingsScreen } from '../screens/settings/SettingsScreen';
+import { ProductScreen } from '../screens/products/ProductScreen';
 
 export type RootStackParams = {
   Home: undefined;
   Products: undefined;
   Settings: undefined;
+  Product: undefined;
 };
 
 const Stack = createNativeStackNavigator<RootStackParams>();
 
 export const StackNavigator = () => {
   return (
-    <Stack.Navigator initialRouteName="Home">
+    <Stack.Navigator screenOptions={ {
+      headerShown: true,
+      headerShadowVisible: false,
+      headerTitleAlign: 'center',
+    } }
+      
+      initialRouteName="Home">
       <Stack.Screen name="Home" component={HomeScreen} />
       <Stack.Screen name="Products" component={ProductsScreen} />
+      <Stack.Screen name="Product" component={ProductScreen} />
       <Stack.Screen name="Settings" component={SettingsScreen} />
     </Stack.Navigator>
   );
